b47ccfff303f26385b3d6f215b99d8bb807da12f
Slawomir Olko <sawek111@gmail.com>
2026-02-22 10:59:24 +0100

    removed not used

--- army/src/army/gateway_bridge.py
+++
@@ -1,9 +0,0 @@
-from grpc.grpc_client import AsyncGatewayGrpcClient
-
-
-class GatewayBridge:
-    def __init__(self):
-        self._client = AsyncGatewayGrpcClient()
-
-    def call_gateway(self, text: str) -> str:
-        return self._client.send(text)
