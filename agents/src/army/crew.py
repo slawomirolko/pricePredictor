89c92ab20e0120c24fa687ee49258ed73906f4d6
Slawomir Olko <sawek111@gmail.com>
2026-02-22 11:31:04 +0100

    Tracing added to crewa

--- agents/src/army/crew.py
+++ agents/src/army/crew.py
@@ -62,5 +62,6 @@ class Army():
             tasks=self.tasks, # Automatically created by the @task decorator
             process=Process.sequential,
             verbose=True,
+            tracing=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
